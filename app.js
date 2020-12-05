72c759f751b3a22bca69cfe4c8360a9bf0e77bb9
guzlej <martin.guzlej@gmail.com>
2020-12-05 15:59:16 +0000

    Saving to [] LS and getRecipeById Function

--- app.js
+++ app.js
@@ -3,27 +3,41 @@ const randomRecipe = document.querySelector('#randomRecipe');
 const getRandomRecipe = async () => {
     const config = { headers: {Accept:'application/json'} }
     const res = await axios.get("https://www.themealdb.com/api/json/v1/1/random.php", config);
-    console.log(res.data.meals[0].strMealThumb)
-
     randomRecipe.innerHTML = `
         <div class="absolute bg-white top-2 p-2 opacity-90"><span class="opacity-100">Random Recipe</span></div>
         <img src="${res.data.meals[0].strMealThumb}" alt="">
         <div class="flex justify-between p-5">
             <span>${res.data.meals[0].strMeal}</span>
-            <button> <i class="fas fa-heart"></i></button>
+            <button><i id="fav-btn" class="fas fa-heart opacity-25"></i></button>
         </div>
-    `
+    `;
+
+    // Add to local storage with click
+    
+    const btn = document.querySelector('#fav-btn');
+    
+    btn.addEventListener('click', (e) => {
+        if (btn.classList.contains('opacity-100')) {
+            removeFromLS(res.data.meals[0].idMeal)
+            btn.classList.remove('opacity-100');
+            btn.classList.add('opacity-25');
+        } else {
+            addToLS(res.data.meals[0].idMeal)
+            btn.classList.add('opacity-100');
+            btn.classList.remove('opacity-25');
+        }
+    })
+    
 }
+
 getRandomRecipe()
-// function getRecipeById(id) {
-//     axios.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772" + id)
-//     .then( res => {
-//         console.log(res.data)
-//     })
-//     .catch(err => {
-//         console.log("Error!", err)
-//     })
-// }
+
+const getRecipeById = async (id) => {
+    const config = { headers: {Accept:'application/json'} }
+    const res = await axios.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id, config);
+    const meal = res.data.meals;
+    return meal
+}
 // function getRecipeBySearch(term) {
 //     axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiatssa" + term)
 //     .then( res => {
@@ -33,3 +47,38 @@ getRandomRecipe()
 //         console.log("Error!", err)
 //     })
 // }
+
+
+function addToLS(recipeId) {
+    const recipeIds = getFromLS();
+
+    localStorage.setItem(
+        'recipeIds',
+        JSON.stringify([...recipeIds, recipeId]));
+}
+
+function removeFromLS(recipeId) {
+    const recipeIds = getFromLS();
+
+    localStorage.setItem(
+        'recipeIds',
+        JSON.stringify(recipeIds.filter((id) => id !== recipeId)));
+}
+
+function getFromLS() {
+    const recipeIds = JSON.parse(localStorage.getItem('recipeIds'));
+
+    return recipeIds === null ? [] : recipeIds;
+}
+fetchFavRecipe()
+async function fetchFavRecipe() {
+    const recipeIds = getFromLS();
+
+    const recipes = [];
+
+    for(let i=0; i<recipeIds.length; i++) {
+        const recipeId = recipeIds[i];
+        recipe = await getRecipeById(recipeId);
+        recipes.push(recipe);
+    }
+}
